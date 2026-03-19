6f5116ac232f5df28bd1e62cf72447f5cc75504d
Ameer Hamza Khetran <hamzaa803@gmail.com>
2026-03-19 08:23:10 +0500

    feat(frontend): add login, signup components
    add login and signup components.
    
    Changes:
    - add background image in public/images
    - add signup, login css in index.css
    - add signup component in src/signup/index.js
    - add login component in src/login/index.js
    - read user values from input fields in signup via useState
    - read user values from input fields in login via useState
    
    Notes:
    - Bullet 1
    - Bullet 2
    - Bullet 3

--- client/src/pages/signup/index.js
+++ client/src/pages/signup/index.js
@@ -1,4 +1,56 @@
+import React from "react"
+import { Link } from "react-router-dom";
+
 function Signup() {
-    return <h2>This is Sign up page.</h2>
+    const [user, setUser] = React.useState({
+        firstname: '',
+        lastname: '',
+        email: '',
+        password: ''
+    })
+
+    function onFormSubmit(event) {
+        event.preventDefault();
+        console.log(user)
+    }
+    return (
+        <div className="container">
+        <div className="container-back-img"></div>
+        <div className="container-back-color"></div>
+        <div className="card">
+            <div className="card-title">
+                <h1>Create Account</h1>
+            </div>
+            <div className="form">
+                <form onSubmit={onFormSubmit}>
+                    <div className="column">
+                        <input type="text" placeholder="First Name" 
+                            value={user.firstname}
+                            onChange={(e) => setUser({...user, firstname: e.target.value})}
+                        />
+                        <input type="text" placeholder="Last Name" 
+                            value={user.lastname}
+                            onChange={(e) => setUser({...user, lastname: e.target.value})}
+                        />
+                    </div>
+                    <input type="email" placeholder="Email" 
+                        value={user.email}
+                        onChange={(e) => setUser({...user, email: e.target.value})}
+                    />
+                    <input type="password" placeholder="Password" 
+                        value={user.password}
+                        onChange={(e) => setUser({...user, password: e.target.value})}
+                    />
+                    <button>Sign Up</button>
+                </form>
+            </div>
+            <div className="card_terms">
+                <span>Already have an account?
+                    <Link to={"/login"}>Login Here</Link>
+                </span>
+            </div>
+        </div>
+    </div>
+    )
 }
 export default Signup
