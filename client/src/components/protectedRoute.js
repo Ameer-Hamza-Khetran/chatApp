ef58fc8cfc5dd68ac861675d5d9bb6eda7a5b622
Ameer Hamza Khetran <hamzaa803@gmail.com>
2026-03-23 05:24:57 +0500

    feat(frontend): display currently logged in user.
    display currently logged in user on frontend by
    making an api call
    
    Changes:
    - create users.js in src/apiCalls
    - make api call to '/api/user/get-logged-user' to
    fetch currently logged in user.
    - call the function to get user, save in state, and
    display on frontend in src/components/protectedRoute.js

--- client/src/components/protectedRoute.js
+++ client/src/components/protectedRoute.js
@@ -1,11 +1,30 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { getLoggedUser } from "../apiCalls/users";
 
 function ProtectedRoute({children}) {
+    const [user, setUser] = useState(null)
     const navigate = useNavigate();
+
+    const getLoggedInUser = async () => {
+        let response = null;
+        try {
+            response = await getLoggedUser();
+            if (response.success) {
+                setUser(response.data)
+            } else {
+                navigate('/login');
+            }
+        } catch (error) {
+            navigate('/login');
+        }
+
+    }
+
     useEffect(() => {
         if (localStorage.getItem('token')) {
             // logic to get details of current user
+            getLoggedInUser()
             
         } else {
             navigate('/login')
@@ -13,6 +32,9 @@ function ProtectedRoute({children}) {
     })
     return (
             <div>
+                <p>Name: {user?.firstname + ' ' + user?.lastname}</p>
+                <p>Email: {user?.email}</p>
+                <br></br>
                 {children}
             </div>
         )
