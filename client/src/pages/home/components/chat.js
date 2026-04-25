a897597c16b2a4072aa097b63dfb43cd5ab3f2e4
Ameer Hamza Khetran <hamzaa803@gmail.com>
2026-04-25 17:35:19 +0500

    resolved ui errors

--- client/src/pages/home/components/chat.js
+++ client/src/pages/home/components/chat.js
@@ -92,7 +92,7 @@ function ChatArea() {
 
     useEffect(() => {
         getMessages();
-        if (selectedChat.lastMessage.sender !== user._id) {
+        if (selectedChat?.lastMessage?.sender !== user._id) {
             clearUnreadMessages();
         }
     }, [selectedChat])
@@ -112,7 +112,12 @@ function ChatArea() {
                                 <div className="message-container" style={isCurrentUserSender? {justifyContent: 'end'} : {justifyContent: 'start'} }>
                                     <div>
                                         <div className={isCurrentUserSender? "send-message" : "received-message"}>{msg.text}</div>
-                                        <div className="message-timestamp" style={isCurrentUserSender? {float: 'right'} : {float: 'left'}}>{formatTime(msg.createdAt)}</div>
+                                        <div className="message-timestamp" style={isCurrentUserSender? {float: 'right'} : {float: 'left'}}>
+                                            {formatTime(msg.createdAt)}
+                                            {isCurrentUserSender && msg.read &&
+                                                <i className="fa fa-check-circle" aria-hidden="true" style={{color: "#e74c3c"}}></i>
+                                            }
+                                        </div>
                                     </div>
                                 </div>
                                 )
